c2cc40143a5002a4af4635237c28e7de6835fd52
Yp251218 <yash.pokharna@indexnine.com>
2024-01-24 12:59:19 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path')
+const morgan = require('morgan');
+
+const app = express();
+const port = 5000;
+
+const logFile = path.join(__dirname, 'access.log');
+
+app.use(morgan('combined', {stream : fs.createWriteStream(logFile,{flags: 'a'})}));
+app.use(morgan('common'));
+app.use(morgan('dev'));
+app.use(morgan('tiny'));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+app.get('/', (req,res) =>{
+    res.send("Hello World")
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
